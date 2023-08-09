d4266ca871c0fb72e7660e94c5cc35785f549232
chaud1024 <chaud1024@gmail.com>
2023-08-09 15:36:26 +0900

    #14-8로그인: 유저 정보 컴포넌트 생성

---
+++ src/components/Header/User.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function User({ user: { photoURL, displayName } }) {
+  return (
+    <div className="flex items-center">
+      <img
+        src={photoURL}
+        alt={displayName}
+        className="w-8 rounded-full md:mr-2"
+      />
+      <span className="hidden md:block">{displayName}</span>
+    </div>
+  );
+}
