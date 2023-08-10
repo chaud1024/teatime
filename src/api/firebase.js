39b9b6c511c5d5bbef7ba48ac8309a942e2a3d47
chaud1024 <chaud1024@gmail.com>
2023-08-10 16:20:27 +0900

    #14-16 firebase에 데이터 쓰기 api 추가

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -6,7 +6,8 @@ import {
   signInWithPopup,
   signOut,
 } from "firebase/auth";
-import { get, getDatabase, ref } from "firebase/database";
+import { get, getDatabase, ref, set } from "firebase/database";
+import { v4 as uuid } from "uuid";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -62,4 +63,15 @@ async function adminUser(user) {
     .catch((error) => {
       console.error(error);
     });
+}
+
+export async function addNewProduct(product, imageUrl) {
+  const id = uuid();
+  set(ref(database, `products/${id}`), {
+    ...product,
+    id,
+    price: parseInt(product.price),
+    image: imageUrl,
+    options: product.options.split(","),
+  });
 }
