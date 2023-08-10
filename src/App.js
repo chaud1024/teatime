7fae2da003457ff78b7542862812dbf85dd4dd90
chaud1024 <chaud1024@gmail.com>
2023-08-10 11:22:42 +0900

    #14-12 로그인Context App에 적용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header/Header";
+import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
   return (
-    <>
+    <AuthContextProvider>
       <Header />
       <Outlet />
-    </>
+    </AuthContextProvider>
   );
 }
 
