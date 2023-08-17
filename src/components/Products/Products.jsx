49d9d0343297c5da2f34a1093f15511e390908b6
chaud1024 <chaud1024@gmail.com>
2023-08-17 18:03:48 +0900

    #14-29 커스텀 훅으로 분리한 react query mutation useProducts를 get, add&update에 적용

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,14 +1,9 @@
-import { useQuery } from "@tanstack/react-query";
 import React from "react";
-import { getProducts } from "../../api/firebase";
+import useProducts from "../../hooks/useProducts";
 import ProductCard from "./ProductCard";
 
 export default function Products() {
-  const {
-    isLoading,
-    data: products,
-    error,
-  } = useQuery(["products"], () => getProducts());
+  const { productsQuery: isLoading, error, data: products } = useProducts();
 
   return (
     <>
--- src/page/AddProducts.jsx
+++ src/page/AddProducts.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
-import { addNewProduct } from "../api/firebase";
 import { uploadImage } from "../api/uploader";
 import LableInput from "../components/Form/LableInput";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import useProducts from "../hooks/useProducts";
 
 export default function AddProducts() {
   const [product, setProduct] = useState({});
@@ -10,13 +9,7 @@ export default function AddProducts() {
   const [isUploading, setIsUploading] = useState(false);
   const [success, setSuccess] = useState();
 
-  const queryClient = useQueryClient();
-  const addProduct = useMutation(
-    ({ product, url }) => addNewProduct(product, url),
-    {
-      onSuccess: () => queryClient.invalidateQueries(["products"]),
-    },
-  );
+  const { addProduct } = useProducts();
 
   const handleChange = (e) => {
     const { name, value, files } = e.target;
