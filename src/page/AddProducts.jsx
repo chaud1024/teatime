ada4414956d49c5a59f4ff4df038457e8e8efb61
chaud1024 <chaud1024@gmail.com>
2023-08-17 16:39:29 +0900

    #14-28 react query useMutation을 사용하여 제품 추가 로직 리팩토링
    addNewProduct api를 직접 사용하지 않고 useMutation을 사용한다. 첫번째로 콜백함수 사용 -> addNewProduct api가 사용하는 인자인 product, url을 객체로 받아서 addNewProduct api를 실행하고, Mutation의 side effect로 성공했을 때(즉 onSuccess) invalidate cache를 하는데, 전부 다 하는 게 아니라 products라는 키를 가진 cache를 invalidate(캐시 무효화)한다

--- src/page/AddProducts.jsx
+++ src/page/AddProducts.jsx
@@ -2,12 +2,21 @@ import React, { useState } from "react";
 import { addNewProduct } from "../api/firebase";
 import { uploadImage } from "../api/uploader";
 import LableInput from "../components/Form/LableInput";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export default function AddProducts() {
   const [product, setProduct] = useState({});
   const [file, setFile] = useState();
   const [isUploading, setIsUploading] = useState(false);
   const [success, setSuccess] = useState();
+
+  const queryClient = useQueryClient();
+  const addProduct = useMutation(
+    ({ product, url }) => addNewProduct(product, url),
+    {
+      onSuccess: () => queryClient.invalidateQueries(["products"]),
+    },
+  );
 
   const handleChange = (e) => {
     const { name, value, files } = e.target;
@@ -23,12 +32,17 @@ export default function AddProducts() {
     setIsUploading(true);
     uploadImage(file)
       .then((url) => {
-        addNewProduct(product, url).then(() => {
-          setSuccess("성공적으로 제품이 추가되었습니다.");
-          setTimeout(() => {
-            setSuccess(null);
-          }, 4000);
-        });
+        addProduct.mutate(
+          { product, url },
+          {
+            onSuccess: () => {
+              setSuccess("성공적으로 제품이 추가되었습니다.");
+              setTimeout(() => {
+                setSuccess(null);
+              }, 4000);
+            },
+          },
+        );
       })
       .finally(() => {
         setIsUploading(false);
