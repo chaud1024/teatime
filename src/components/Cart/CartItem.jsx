775149b128a81f42565a7c6e3e2ef6b7f2d7dbe0
chaud1024 <chaud1024@gmail.com>
2023-08-17 16:36:00 +0900

    #14-28 cart, cart item css 수정

--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -3,6 +3,8 @@ import { AiOutlineMinusSquare, AiOutlinePlusSquare } from "react-icons/ai";
 import { BsTrash3 } from "react-icons/bs";
 import { addOrUpdateToCart, removeFromCart } from "../../api/firebase";
 
+const ICON_CLASS = "cursor-pointer hover:scale-105 text-lg";
+
 export default function ProductItem({
   product,
   product: { id, image, title, option, quantity, price },
@@ -17,16 +19,29 @@ export default function ProductItem({
   const handleDelete = () => removeFromCart(uid, id);
 
   return (
-    <li>
-      <img src={image} alt={title} />
-      <div>
-        <p>{title}</p>
-        <p>{option}</p>
+    <li className="flex gap-6 pb-6 mb-6 border-b border-gray-200">
+      <div className="basis-1/6 overflow-hidden">
+        <img src={image} alt={title} className="w-full" />
+      </div>
+      <div className="flex flex-col justify-between grow">
         <div>
-          <AiOutlineMinusSquare onClick={handleMinus} />
-          <span>{quantity}</span>
-          <AiOutlinePlusSquare onClick={handlePlus} />
-          <BsTrash3 onClick={handleDelete} />
+          <p className="tracking-widest font-semibold">{title}</p>
+          <p className="text-gray-600 my-2">€{price} Per 100g</p>
+          <p className="font-semibold">Ordered: {option}</p>
+        </div>
+        <div className="flex items-center justify-between">
+          <p className="font-semibold">
+            €{(parseFloat(price) * parseInt(option)) / 100}
+          </p>
+          <div className="flex items-center gap-2">
+            <AiOutlineMinusSquare
+              onClick={handleMinus}
+              className={ICON_CLASS}
+            />
+            <span>{quantity}</span>
+            <AiOutlinePlusSquare onClick={handlePlus} className={ICON_CLASS} />
+            <BsTrash3 onClick={handleDelete} className={ICON_CLASS} />
+          </div>
         </div>
       </div>
     </li>
--- src/components/Cart/PriceCard.jsx
+++ src/components/Cart/PriceCard.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
-export default function PriceCard({ text, product }) {
-  return <div>PriceCard</div>;
+export default function PriceCard({ text, price, final, delivery }) {
+  return (
+    <div className="w-full flex justify-between items-center py-6 border-b border-gray-200">
+      <p className="text-sm text-gray-600">
+        {text}
+        <span className="text-red-500">
+          {delivery ? <p>* Free shipping over €50</p> : null}
+        </span>
+      </p>
+      <p className={`font-bold ${final ? "text-xl" : ""}`}>€{price}</p>
+    </div>
+  );
 }
