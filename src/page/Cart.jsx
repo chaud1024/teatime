745f2e7f71ad578d46931792f56f52d8c5412fb9
chaud1024 <chaud1024@gmail.com>
2023-08-17 16:35:32 +0900

    #14-28 상품총액 수식 변경

--- src/page/Cart.jsx
+++ src/page/Cart.jsx
@@ -1,13 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
-import { FaEquals } from "react-icons/fa";
-import { HiPlusSm } from "react-icons/hi";
 import { getCart } from "../api/firebase";
-import PriceCard from "../components/Cart/PriceCard";
 import CartItem from "../components/Cart/CartItem";
+import PriceCard from "../components/Cart/PriceCard";
 import { useAuthContext } from "../context/AuthContext";
 
-const SHIPPING = 3000;
+const SHIPPING = 3.2;
 
 export default function Cart() {
   const { uid } = useAuthContext();
@@ -16,31 +14,49 @@ export default function Cart() {
   if (isLoading) return <p>Loading...</p>;
 
   const hasProducts = products && products.length > 0;
+
   const totalPrice =
     products &&
     products.reduce(
-      (prev, current) => prev + parseFloat(current.price) * current.quantity,
+      (prev, current) =>
+        prev +
+        ((parseFloat(current.price) * parseInt(current.option)) / 100) *
+          current.quantity,
       0,
     );
   return (
-    <section>
-      <p>My Cart</p>
+    <section className="">
+      <p className="font-bold text-2xl tracking-widest pb-4  mx-6 my-4 border-b-2 border-black">
+        My Cart
+      </p>
       {!hasProducts && <p>Empty cart</p>}
       {hasProducts && (
         <>
-          <ul>
+          <ul className="mt-14 mx-6">
             {products &&
               products.map((product) => (
                 <CartItem key={product.id} product={product} uid={uid} />
               ))}
           </ul>
 
-          <div>
-            <PriceCard text="상품 총액" price={totalPrice} />
-            <HiPlusSm />
-            <PriceCard text="배송액" price={SHIPPING} />
-            <FaEquals />
-            <PriceCard text="총가격" price={totalPrice + SHIPPING} />
+          <div className="flex flex-col items-center px-6 pb-6 bg-gray-100">
+            <PriceCard
+              text="Total products price incl. VAT"
+              price={totalPrice}
+            />
+            <PriceCard
+              text="Estimated delivery"
+              price={totalPrice > 50 ? 0 : SHIPPING}
+              delivery
+            />
+            <PriceCard
+              text="Total price incl. VAT"
+              price={totalPrice + SHIPPING}
+              final
+            />
+            <button className="uppercase font-bold bg-black tracking-widest text-white w-full py-4 inline-block">
+              proceed to checkout
+            </button>
           </div>
         </>
       )}
