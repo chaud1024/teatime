dba9900dcd4b96b449182f6829350bec1b6a613e
chaud1024 <chaud1024@gmail.com>
2023-08-18 17:57:28 +0900

    filter test

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,12 +3,20 @@ import useProducts from "../../hooks/useProducts";
 import ProductCard from "./ProductCard";
 
 export default function Products() {
-  const { productsQuery: isLoading, error, data: products } = useProducts();
+  const {
+    productsQuery: { isLoading, error, data: products },
+  } = useProducts();
 
+  const filteredProducts = products.filter(
+    (item) => item.category === "Black tea",
+  );
+
+  console.log(filteredProducts);
   return (
     <>
       {isLoading && <p>Loading...</p>}
       {error && <p>{error}</p>}
+
       <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 lg:gap-4 p-4 mt-8">
         {products &&
           products.map((product) => (
