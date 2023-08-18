f95955a36eaafce3a34bf115ae1a54b068dca725
chaud1024 <chaud1024@gmail.com>
2023-08-18 16:47:23 +0900

    #14-31 오류 수정

--- src/components/Cart/PriceCard.jsx
+++ src/components/Cart/PriceCard.jsx
@@ -3,12 +3,14 @@ import React from "react";
 export default function PriceCard({ text, price, final, delivery }) {
   return (
     <div className="w-full flex justify-between items-center py-6 border-b border-gray-200">
-      <p className="text-sm text-gray-600">
-        {text}
-        <span className="text-red-500">
-          {delivery ? <p>* Free shipping over €50</p> : null}
-        </span>
-      </p>
+      <div>
+        <p className="text-sm text-gray-600">{text}</p>
+        {delivery ? (
+          <span className="text-sm text-red-500">
+            <p>* Free shipping over €50</p>
+          </span>
+        ) : null}
+      </div>
       <p className={`font-bold ${final ? "text-xl" : ""}`}>€{price}</p>
     </div>
   );
